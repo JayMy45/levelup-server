c2bdc9f5b6bff8548e1f6a5a75e5055303ce138c
Jeremy Myers <echelonical@gmail.com>
2022-10-31 15:23:45 -0500

    updated class models

--- levelupapi/models/game_type.py
+++ levelupapi/models/game_type.py
@@ -1,4 +1,5 @@
 from django.db import models
 
 class Type(models.Model):
+    
     label = models.CharField(max_length=50)
--- levelupapi/models/gamer_event.py
+++ levelupapi/models/gamer_event.py
@@ -1,5 +1,6 @@
 from django.db import models
 
 class GamerEvent(models.Model):
+    
     event = models.ForeignKey("Event", on_delete=models.CASCADE, related_name='submitted_tickets')
     gamer = models.ForeignKey("Gamer", on_delete=models.CASCADE, related_name='submitted_tickets')
