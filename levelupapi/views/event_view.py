57c50257be6786d9358ffd1b2135e84e34ba513f
Jeremy Myers <echelonical@gmail.com>
2022-11-07 11:23:57 -0600

    add action decorator to EventView to facility gamer joining an event

--- levelupapi/views/event_view.py
+++ levelupapi/views/event_view.py
@@ -1,5 +1,6 @@
 """View module for handling requests about game types"""
 from django.http import HttpResponseServerError
+from rest_framework.decorators import action
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
@@ -10,6 +11,17 @@ from levelupapi.models import Event, Game, Gamer
 
 class EventView(ViewSet):
     """Level up game types view"""
+
+    @action(methods=['post'], detail=True)
+    def signup(self, request, pk):
+        """Post request for a user to sign up for an event"""
+    
+        gamer = Gamer.objects.get(user=request.auth.user)
+        event = Event.objects.get(pk=pk)
+        event.attendees.add(gamer)
+        return Response({'message': 'Gamer added'}, status=status.HTTP_201_CREATED)
+    
+
 
     def retrieve(self, request, pk):
         """Handle GET requests for single game type
@@ -97,4 +109,4 @@ class EventSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Event
-        fields = ('id', 'organizer', 'game', 'description','organizer', 'date', 'time', )
+        fields = ('id', 'organizer', 'game', 'description','organizer', 'date', 'time', 'attendees' )
